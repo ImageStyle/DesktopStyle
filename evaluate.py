e86512bc7973dfcc6b1233f2b54015fcb33985ef
Ian Johnson <ianjjohnson@mac.com>
2017-10-03 15:31:32 -0500

    create Recording branch for pre-recording and styling images for basic video testing (to make sure models produce good video)

--- evaluate.py
+++ evaluate.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from argparse import ArgumentParser
 from collections import defaultdict
 import numpy as np
+import time
 
 DEVICE = '/gpu:0'
 
@@ -30,24 +31,38 @@ def ffwd(checkpoint_dir, device_t='/gpu:0'):
         import cv2
         video_stream = cv2.VideoCapture(0)
 
-        while(True):
+        input_imgs = []
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        for i in range(60):
+
+            start = time.time()
 
             ret, frame = video_stream.read()
 
-            img = cv2.resize(frame, (img_shape[0],img_shape[1]))
-            cv2.imshow('frame', img)
-            #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            input_imgs.append(cv2.resize(frame, (img_shape[0],img_shape[1])))
+            time.sleep(max(1./30 - (time.time() - start), 0))
 
-            X = [img]
+        print("Collection done")
+        start = time.time()
 
-            _preds = sess.run(preds, feed_dict={img_placeholder:X})
-            # save_img(path_out, _preds[j
-            #print(_preds[0].shape)
-            result = np.clip(_preds[0], 0, 255).astype(np.uint8)
-            cv2.imshow('frame', cv2.resize(result, (img_shape[0],img_shape[1])))
+        output_imgs = []
+
+        for frame in input_imgs:
+
+            _preds = sess.run(preds, feed_dict={img_placeholder:[frame]})
+            output_imgs.append(np.clip(_preds[0], 0, 255).astype(np.uint8))
+
+        print("Style transfer done. Time: " + str(time.time() - start))
+
+        cv2.imshow('frame', cv2.resize(output_imgs[0], (img_shape[0],img_shape[1])))
+
+        while(True):
+            for frame in output_imgs:
+                start = time.time()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    return
+                cv2.imshow('frame', cv2.resize(frame, (img_shape[0],img_shape[1])))
+                time.sleep(max(1./30 - (time.time() - start), 0))
 
 def build_parser():
     parser = ArgumentParser()
